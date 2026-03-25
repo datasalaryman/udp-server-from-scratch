67fd76e96e5d6023776600dfbec0b2dcd733169e
Data Salaryman <datasalaryman@protonmail.com>
2026-03-25 14:19:59 +0800

    allow for true broadcast

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
-use std::{os::fd::RawFd, str::FromStr};
+use std::{os::fd::RawFd, str::FromStr, time::Duration};
 use nix::sys::socket::{
    AddressFamily, Backlog, MsgFlags, SockFlag, SockType, SockaddrIn, accept, bind, listen, recv, recvfrom, send, sendto, setsockopt, socket, sockopt::{Broadcast, ReuseAddr}
 };
 use nix::unistd::{close};
 use std::os::fd::AsRawFd;
-
+use std::thread::sleep;
 
 pub fn run() {
     let sock_addr = SockaddrIn::from_str("0.0.0.0:3000").unwrap(); 
@@ -16,19 +16,17 @@ pub fn run() {
     
     bind(fd.as_raw_fd(), &sock_addr).unwrap();
 
-    let mut buf = [0u8; 1024];
+    let broadcast = SockaddrIn::from_str("255.255.255.255:3001").unwrap();
+
+    let buf = "Hello from server\r\n".as_bytes();
             
     loop {
-        let (bytes, client) = recvfrom(fd.as_raw_fd(), &mut buf).unwrap();
         
+        sendto(fd.as_raw_fd(), &buf, &broadcast, MsgFlags::empty()).unwrap();
+        sleep(Duration::from_millis(100));
+    };
 
-        let client_addr : SockaddrIn = client.unwrap();
-    
+    // close(fd.as_raw_fd()).unwrap(); 
 
-        println!("Client Address: {:?}, Port: {:?}", &client_addr.ip(), &client_addr.port());
-        println!("File descriptor: {:?}", &fd);
-
-        // close(fd.as_raw_fd()).unwrap(); 
-    }
 
 }
